544756747ea2351dbe31dc029e54822b391c57fb
Nacu George <nacu_george@hotmail.com>
2022-09-03 12:15:08 +0300

    Updates

---
+++ precache-manifest.0aa84ae376502bd45579e62bba68bb68.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a57e32a9270d660fa594",
+    "url": "/Company-Staff/static/css/main.dae558e4.chunk.css"
+  },
+  {
+    "revision": "a57e32a9270d660fa594",
+    "url": "/Company-Staff/static/js/main.814e7605.chunk.js"
+  },
+  {
+    "revision": "708eb8ac41bb1fdec8b3",
+    "url": "/Company-Staff/static/js/runtime~main.6e71e1c5.js"
+  },
+  {
+    "revision": "731a4ba9f6c54ecda29a",
+    "url": "/Company-Staff/static/css/2.614a9104.chunk.css"
+  },
+  {
+    "revision": "731a4ba9f6c54ecda29a",
+    "url": "/Company-Staff/static/js/2.fd0ca411.chunk.js"
+  },
+  {
+    "revision": "9c19088db84563606874",
+    "url": "/Company-Staff/static/js/3.5526bd81.chunk.js"
+  },
+  {
+    "revision": "f6bd51babe7f8dda6b0b15de969a0377",
+    "url": "/Company-Staff/index.html"
+  }
+];
